57b69660e88743b7912c5ef287547e0771d6080d
Настя Скрицька <snastia.m@gmail.com>
2023-06-05 11:18:52 +0300

    укаыцуаца

---
+++ 17-vars.js
@@ -0,0 +1,7 @@
+const refs = {
+    parent: document.querySelector("#parent"),
+    child: document.querySelector("#child"),
+    innerChild: document.querySelector("#inner-child"),
+}
+
+// щоб зупинити подію - stopImmediatePropagation()
